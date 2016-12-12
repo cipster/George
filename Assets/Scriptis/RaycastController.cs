f0bdcb831d43f471d4bec24eae6dd98abf94ab8b
cipster <bostanciprian@gmail.com>
2016-12-12 08:26:07 +0200

    Initial commit

---
+++ Assets/Scriptis/RaycastController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class RaycastController : MonoBehaviour {
+    [HideInInspector]
+    public BoxCollider2D collider;
+    public RayCastOrigins rayCastOrigins;
+    public LayerMask collisionMask;
+    const float distanceBetweenRays = .25f;
+    [HideInInspector]
+    public int horizontalRayCount;
+    [HideInInspector]
+    public int verticalRayCount;
+
+    [HideInInspector]
+    public float horizontalRaySpacing;
+    [HideInInspector]
+    public float verticalRaySpacing;
+
+    public const float skinWidth = .015f;
+
+    public virtual void Awake() {
+        collider = GetComponent<BoxCollider2D>();
+    }
+
+    public virtual void Start() {
+        CalculateRaySpacing();
+    }
+
+    public void CalculateRaySpacing() {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        float boundsWidth = bounds.size.x;
+        float boundsHeight = bounds.size.y;
+
+        horizontalRayCount = Mathf.RoundToInt(boundsHeight / distanceBetweenRays);
+        verticalRayCount = Mathf.RoundToInt(boundsWidth / distanceBetweenRays);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRayCount - 1);
+    }
+
+    public void UpdateRayCastOrigins() {
+        Bounds bounds = collider.bounds;
+        bounds.Expand(skinWidth * -2);
+
+        rayCastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
+        rayCastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
+        rayCastOrigins.topLeft = new Vector2(bounds.min.x, bounds.max.y);
+        rayCastOrigins.topRight = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+    public struct RayCastOrigins {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    }
+}
